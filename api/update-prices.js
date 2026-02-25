87e8fa8fc60742aae4e2aadc1e7294d30ef5c121
Jeffreypool <jeffrey-pool@hotmail.com>
2026-02-25 14:24:48 +0100

    Update update-prices.js

--- api/update-prices.js
+++ api/update-prices.js
@@ -1,35 +1,5 @@
-// Zoek prijs bij "From"
-const fromBlock = html.split('From</dt>')[1];
-
-if (!fromBlock) {
-  results.push({
-    item: item.name,
-    status: 'No price found'
-  });
-  continue;
-}
-
-// Pak eerste € bedrag na From
-const euroMatch = fromBlock.match(/([\d\.,]+)\s?€/);
-
-if (!euroMatch) {
-  results.push({
-    item: item.name,
-    status: 'No price found'
-  });
-  continue;
-}
-
-const price = parseFloat(
-  euroMatch[1]
-    .replace(/\./g, '')
-    .replace(',', '.')
-);
-
-if (isNaN(price)) {
-  results.push({
-    item: item.name,
-    status: 'Invalid price format'
-  });
-  continue;
+export default function handler(req, res) {
+  return res.status(200).json({
+    step: "function reached"
+  })
 }
