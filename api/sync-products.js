9035bb5670b9e845da1bcc4203722f8b4c5ff14e
Jeffreypool <jeffrey-pool@hotmail.com>
2026-03-09 13:59:03 +0100

    Update sync-products.js

--- api/sync-products.js
+++ api/sync-products.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     const apiData = await response.json()
 
 return res.status(200).json(apiData)
-     }
+     
     for (const product of products) {
 
       const price = product.prices?.cardmarket?.lowest || null
