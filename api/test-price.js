a6e7e289a00304d9497408b48e2377fdafa35357
Jeffreypool <jeffrey-pool@hotmail.com>
2026-03-09 10:19:34 +0100

    Update test-price.js

--- api/test-price.js
+++ api/test-price.js
@@ -1,18 +1,3 @@
-export default async function handler(req, res) {
-
-  const response = await fetch(
-    "https://pokemon-tcg-api.p.rapidapi.com/cards?per_page=1&page=1",
-    {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-        "X-RapidAPI-Host": "pokemon-tcg-api.p.rapidapi.com"
-      }
-    }
-  );
-
-  const data = await response.json();
-
-  res.json(data);
-
+export default function handler(req, res) {
+  res.status(200).json({ message: "API werkt" });
 }
