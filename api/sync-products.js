409df998299153cab9de924a1a5425cb8142803d
Jeffreypool <jeffrey-pool@hotmail.com>
2026-03-09 14:13:33 +0100

    Update sync-products.js

--- api/sync-products.js
+++ api/sync-products.js
@@ -7,28 +7,25 @@ const supabase = createClient(
 
 export default async function handler(req, res) {
 
-  const response = await fetch(
-    "https://pokemon-prices.p.rapidapi.com/products",
-    {
-      headers: {
-        "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-        "X-RapidAPI-Host": "pokemon-prices.p.rapidapi.com"
+  try {
+
+    const response = await fetch(
+      "https://pokemon-prices.p.rapidapi.com/products",
+      {
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+          "X-RapidAPI-Host": "pokemon-prices.p.rapidapi.com"
+        }
       }
-    }
-  )
+    )
 
-  const data = await response.json()
+    const data = await response.json()
 
-  res.status(200).json(data)
-}
-
-    res.status(200).json({
-      synced_products: products.length
-    })
+    return res.status(200).json(data)
 
   } catch (err) {
 
-    res.status(500).json({
+    return res.status(500).json({
       error: err.message
     })
 
