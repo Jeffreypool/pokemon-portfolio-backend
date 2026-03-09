631d5f08bacd82e523e21781f41a6b71b961da8f
Jeffreypool <jeffrey-pool@hotmail.com>
2026-03-09 10:15:10 +0100

    Update test-price.js

--- api/test-price.js
+++ api/test-price.js
@@ -1,12 +1,15 @@
 export default async function handler(req, res) {
 
-  const response = await fetch("https://pokemon-prices.p.rapidapi.com/product/evolving-skies-booster-box", {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-      "X-RapidAPI-Host": "pokemon-prices.p.rapidapi.com"
+  const response = await fetch(
+    "https://pokemon-tcg-api.p.rapidapi.com/cards?per_page=1&page=1",
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+        "X-RapidAPI-Host": "pokemon-tcg-api.p.rapidapi.com"
+      }
     }
-  });
+  );
 
   const data = await response.json();
 
