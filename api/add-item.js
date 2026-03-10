8aa5f196a53029f90927c08ee6e1fc5a6a35a5e3
Jeffreypool <jeffrey-pool@hotmail.com>
2026-03-10 10:37:42 +0100

    Update add-item.js

--- api/add-item.js
+++ api/add-item.js
@@ -4,7 +4,7 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 )
-
+ 
 export default async function handler(req, res) {
   try {
     if (req.method !== 'POST') {
