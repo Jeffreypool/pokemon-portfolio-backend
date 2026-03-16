75451352e4ee7d233bcd886faf5986a53f1a2aea
Jeffreypool <jeffrey-pool@hotmail.com>
2026-03-16 11:25:08 +0100

    Update add-item.js

--- api/add-item.js
+++ api/add-item.js
@@ -43,6 +43,7 @@ export default async function handler(req, res) {
     if (error) {
       return res.status(500).json({ error: error.message })
     }
+    await supabase.rpc('insert_price_snapshots')
 
     return res.status(200).json({ success: true, item: data[0] })
 
