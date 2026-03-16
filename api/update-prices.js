eac56752f1ebb41658690b5abb5163d8a92e36fd
Jeffreypool <jeffrey-pool@hotmail.com>
2026-03-16 13:30:12 +0100

    Update update-prices.js

--- api/update-prices.js
+++ api/update-prices.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     while (hasMore) {
 
       const response = await fetch(
-        `https://pokemon-tcg-api.p.rapidapi.com/products?page=${page}&per_page=200`,
+        `https://pokemon-tcg-api.p.rapidapi.com/episodes/21/products?page=${page}`,
         {
           headers: {
             "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
